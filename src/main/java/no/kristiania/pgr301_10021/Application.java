03955e217b93fb4c46fa90d7f88cc35b6ed372a6
Daniel <boylan157@gmail.com>
2020-11-13 12:55:13 +0100

    added data to Application

--- src/main/java/no/kristiania/pgr301_10021/Application.java
+++ src/main/java/no/kristiania/pgr301_10021/Application.java
@@ -1,13 +1,54 @@
 package no.kristiania.pgr301_10021;
 
+import no.kristiania.pgr301_10021.model.Ninja;
+import no.kristiania.pgr301_10021.repository.NinjaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	@Autowired
+	CommandLineRunner runner(NinjaRepository ninjaRepository){
+		return (args) -> {
+			long count = ninjaRepository.count();
+
+			if(count == 0){
+				try {
+					Ninja ninja = new Ninja();
+					ninja.setName("Kirito");
+					ninja.setKatana("The Elucidator");
+
+					Ninja ninja2 = new Ninja();
+					ninja2.setName("Asuna");
+					ninja2.setKatana("Lambent Light");
+
+					Ninja ninja3 = new Ninja();
+					ninja3.setName("Klein");
+					ninja3.setKatana("Excalibur");
+
+					Ninja ninja4 = new Ninja();
+					ninja4.setName("testNinja");
+					ninja4.setKatana("TestKatana");
+
+					ninjaRepository.save(ninja);
+					ninjaRepository.save(ninja2);
+					ninjaRepository.save(ninja3);
+					ninjaRepository.save(ninja4);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
 	}
 
 }
