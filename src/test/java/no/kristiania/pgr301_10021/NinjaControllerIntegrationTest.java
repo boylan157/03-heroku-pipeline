f4e113d16f8d9b2921351e833bde440fdd091e0a
Daniel <boylan157@gmail.com>
2020-11-13 13:14:37 +0100

    added docker push

--- src/test/java/no/kristiania/pgr301_10021/NinjaControllerIntegrationTest.java
+++ src/test/java/no/kristiania/pgr301_10021/NinjaControllerIntegrationTest.java
@@ -3,13 +3,13 @@ package no.kristiania.pgr301_10021;
 
 import no.kristiania.pgr301_10021.model.Ninja;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.web.client.HttpClientErrorException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
